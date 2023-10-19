6c58b0b32b3df8e81e204ea9e0090fe52d9cd518
BasselMahmoudD <Bassel@yahoo.com>
2023-10-19 05:30:18 -0700

    Done

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 #include <stdarg.h>
 #define OUTPUT_BUF_SIZE 1024
 #define BUF_FLUSH -1
-#define NULL_STRING "(null)"
+#define NULL_STRING "(NULL)"
 #define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 #define CONVERT_LOWERCASE  1
 #define CONVERT_UNSIGNED  2
@@ -15,15 +15,15 @@
 /**
  * struct parameters - parameters struct
  * @unsign: flag if unsigned value
- * @plus_flag: on if plus_flag specified
- * @space_flag: on if hashtag_fflag
- * @hashtag_flag: on if _flag specified
- * @zero_flag: on if _flag specified
- * @minus_flag: on if _flag specified
- * @width: field width specified
- * @precision: field precision specified
- * @h_modifier: on if h_modifire
- * @l_modifier: on if l_modifier
+ *@plus_flag: on if plus_flag specified
+ *@space_flag: on if hashtag_fflag
+ *@hashtag_flag: on if _flag specified
+ *@zero_flag: on if _flag specified
+ *@minus_flag: on if _flag specified
+ *@width: field width specified
+ *@precision: field precision specified
+ *@h_modifier: on if h_modifire
+ *@l_modifier: on if l_modifier
  *
  */
 
